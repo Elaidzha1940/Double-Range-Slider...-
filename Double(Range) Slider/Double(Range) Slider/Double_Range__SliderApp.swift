a7118da1fc3753e71bd3820b0cdf03ca12740577
Elaidzha1940 <eli080293@gmail.com>
2023-03-20 18:33:05 +0700

    Double(Range) Slider...$

---
+++ Double(Range) Slider/Double(Range) Slider/Double_Range__SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Double_Range__SliderApp.swift
+//  Double(Range) Slider
+//
+//  Created by Elaidzha Shchukin on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Double_Range__SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
