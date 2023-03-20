9e9ebd9c625c7dc8e841259ee73242319cd48073
Elaidzha1940 <eli080293@gmail.com>
2023-03-20 18:44:47 +0700

    Double Slider2 + added Colors

--- Double(Range) Slider/Double(Range) Slider/Double_Range__SliderApp.swift
+++ Double(Range) Slider/Double(Range) Slider/Double_Range__SliderApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Double_Range__SliderApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SliderComponent()
         }
     }
 }
---
+++ Double(Range) Slider/Double(Range) Slider/SliderComponent.swift
@@ -0,0 +1,130 @@
+//
+//  ContentView.swift
+//  Double(Range) Slider
+//
+//  Created by Elaidzha Shchukin on 20.03.2023.
+//
+
+import SwiftUI
+
+struct SliderComponent: View {
+    
+    var totalWidht = UIScreen.main.bounds.width - 80
+    
+    var body: some View {
+        
+        Slider2()
+    }
+}
+
+struct SliderComponent_Previews: PreviewProvider {
+    static var previews: some View {
+            SliderComponent()
+    }
+}
+
+struct Slider2 : View {
+    
+    @State var width: CGFloat = 0
+    @State var width1:  CGFloat = 30
+    var totalWidth = UIScreen.main.bounds.width - 80
+     
+    var right: String {
+        
+        let equal = self.width / self.totalWidth
+        
+        let rub = Int(equal / 000.001000)
+        return "\(rub) р"
+    }
+    
+    var left: String {
+        
+        let equal = self.width1 / self.totalWidth
+        
+        let rub = Int(equal / 0.0003000)
+        return "\(rub) р"
+        
+    }
+
+    var body: some View {
+        
+        VStack {
+            HStack {
+                Text("Ценовой диапазон")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                  
+                Spacer()
+                
+                Text("\(right) - \(left)")
+                    .fontWeight(.bold)
+                    .foregroundColor(.blue)
+            }
+    
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .fill(Color.brown)
+                    .frame(height: 4)
+                
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: self.width1 - self.width, height: 6)
+                    .offset(x: self.width + 28)
+                
+                HStack(spacing: 0) {
+                    
+                    Circle()
+                        .fill(Color.blue)
+                        .frame(width: 28, height: 28)
+                        .offset(x: self.width)
+                        .gesture(
+                            
+                            DragGesture()
+                                .onChanged({ (value) in
+                                    
+                                    if value.location.x >= 0 && value.location.x <= self.width1 {
+                                        self.width = value.location.x
+                                    }
+                                })
+                        )
+                    
+                    Circle()
+                        .fill(Color.brown)
+                        .frame(width: 28, height: 28)
+                        .offset(x: self.width1)
+                        .gesture(
+                            
+                            DragGesture()
+                                .onChanged({ (value) in
+                                    
+                                    if value.location.x <= self.totalWidth && value.location.x >= self.width {
+                                        
+                                        self.width1 = value.location.x
+                                    }
+                                    
+                                })
+                        )
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+
+
+struct Currecny: View {
+    let price: Double = 1000
+    
+    var body: some View {
+        Text(formatCurrency(price: price))
+    }
+    
+    func formatCurrency(price: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "р"
+        formatter.locale = Locale.current
+        return formatter.string(from: NSNumber(value: price)) ?? ""
+    }
+}
